0102aa9410996eb5be910d6536783f3ac4ad38c0
Clara Bueno Rodrigues <clarabuenorodrigues1204@gmail.com>
2026-08-20 02:42:44 -0300

    Ultimo estudo do dia 19/08

--- Exercícios Python Mundo 3/Exercício 016.py
+++ Exercícios Python Mundo 3/Exercício 016.py
@@ -1,2 +1,24 @@
 from os import system
 system('cls')
+
+from random import randint
+from time import sleep
+cont = 0
+palpites = []
+jogos = []
+jogador = int(input('Digite o número de palpites: '))
+
+for i in range(jogador):
+    while True:
+        num = randint(1,60)
+        if num not in palpites:
+            palpites.append(num)
+            cont += 1
+        if cont >= 6:
+            cont = 0
+            break
+    jogos.append(palpites[:])
+    palpites.clear()
+    
+for i, l in enumerate(jogos):
+    print(f'Jogo {i + 1}: {l}')
